eaee5957c28bc091b41cc38906c01c0cf0929933
TianYaKe <liulong@hzins.com>
2016-06-08 20:20:47 +0800

    MA ….CALayer02

---
+++ CALayer--02/CALayer--02Tests/CALayer__02Tests.m
@@ -0,0 +1,39 @@
+//
+//  CALayer__02Tests.m
+//  CALayer--02Tests
+//
+//  Created by long on 6/8/16.
+//  Copyright © 2016 long. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CALayer__02Tests : XCTestCase
+
+@end
+
+@implementation CALayer__02Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
